17dfa6976b250e5639854bedd3c6158876260357
Tobias Mesquita <tobias.mesquita@gmail.com>
2017-10-01 10:52:18 -0300

    Rename variables to english.

--- TobyMosque.Sample.Service.Net.Npgsql/SampleContext.cs
+++ TobyMosque.Sample.Service.Net.Npgsql/SampleContext.cs
@@ -6,8 +6,8 @@ namespace TobyMosque.Sample.Service.Net.Npgsql
     public class SampleContext : Net.SampleContext
     {
         public SampleContext() : base() { }
-        public SampleContext(Guid? sessaoID, int grupoID) : base(sessaoID, grupoID) { }
+        public SampleContext(Guid? sessionId, int tenantId) : base(sessionId, tenantId) { }
         public SampleContext(DbContextOptions options) : base(options) { }
-        public SampleContext(DbContextOptions options, Guid? sessaoID, int grupoID) : base(options, sessaoID, grupoID) { }
+        public SampleContext(DbContextOptions options, Guid? sessionId, int tenantId) : base(options, sessionId, tenantId) { }
     }
 }
